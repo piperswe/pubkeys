4494196b34172f4cb2ddf2c1f2bc002cdd89afd6
Piper McCorkle <contact@piperswe.me>
2022-08-29 11:34:51 -0500

    Reorganize flake

--- flake.nix
+++ flake.nix
@@ -4,42 +4,47 @@
 
   description = "Piper's public cryptographic keys, usable from Nix flakes";
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-    ssh = rec {
-      raw = builtins.readFile ./ssh_public_keys;
-      keys = builtins.split "\n" raw;
-    };
-    pgp = rec {
-      raw = builtins.readFile ./pgp_public_keys;
-    };
-  in
-  rec {
-    lib = {
-      inherit ssh pgp;
-    };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      ssh = rec {
+        raw = builtins.readFile ./ssh_public_keys;
+        keys = builtins.split "\n" raw;
+      };
+      pgp = rec {
+        raw = builtins.readFile ./pgp_public_keys;
+      };
+      perSystem = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          packages.sshAuthorizedKeys = pkgs.writeText "authorized_keys" ssh.raw;
 
-    packages.sshAuthorizedKeys = pkgs.writeText "authorized_keys" ssh.raw;
+          devShells.fetch = pkgs.mkShell {
+            buildInputs = [
+              pkgs.wget
+            ];
+          };
+          devShells.default = devShells.fetch;
+        });
+      root = {
+        lib = {
+          inherit ssh pgp;
+        };
 
-    nixosModules.sshAuthorizedKeys = { config, lib }: {
-      options.piperswe-pubkeys = {
-        enable = lib.mkEnableOption "SSH public keys for Piper";
-        user = lib.mkOption {
-          type = lib.types.str;
-          default = "pmc";
+        nixosModules.sshAuthorizedKeys = { config, lib }: {
+          options.piperswe-pubkeys = {
+            enable = lib.mkEnableOption "SSH public keys for Piper";
+            user = lib.mkOption {
+              type = lib.types.str;
+              default = "pmc";
+            };
+          };
+          config = lib.mkIf config.piperswe-pubkeys.enable {
+            users.user.${config.piperswe-pubkeys.user}.openssh.authorizedKeys.keys = ssh.keys;
+          };
         };
       };
-      config = lib.mkIf config.piperswe-pubkeys.enable {
-        users.user.${config.piperswe-pubkeys.user}.openssh.authorizedKeys.keys = ssh.keys;
-      };
-    };
-
-    devShells.fetch = pkgs.mkShell {
-      buildInputs = [
-        pkgs.wget
-      ];
-    };
-    devShells.default = devShells.fetch;
-  });
+    in
+    root // perSystem;
 }
